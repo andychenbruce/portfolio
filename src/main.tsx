ab19cce0a19240da67577b2734b1d1117497a3e3
陈功 <chengong456@qq.com>
2024-05-31 00:01:03 -0700

    ported contact page

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { buildPage } from './build.js';
 
+import Wrapper from './wrapper.js';
+
 import Index from './pages/index.js';
 import MenuPage from './pages/menu_page.js';
 import AboutPage from './pages/about_page.js';
+
 
 buildPage('index.html', ReactDOMServer.renderToStaticMarkup(
   <Index title="Andy's Project Website" description="Put cool description here" />
@@ -16,4 +19,15 @@ buildPage('menuPage.html', ReactDOMServer.renderToStaticMarkup(
 
 buildPage('aboutPage.html', ReactDOMServer.renderToStaticMarkup(
   <AboutPage title="Andy's Project Website" description="Put cool description here" />
+));
+
+buildPage('aboutPage.html', ReactDOMServer.renderToStaticMarkup(
+  <AboutPage title="Andy's Project Website" description="Put cool description here" />
+));
+
+buildPage("contactPage.html", ReactDOMServer.renderToStaticMarkup(
+  <Wrapper title="Andy's Project Website" description="Put cool description here">
+    <p>Phone number: 14082073992</p>
+    <p>Email: chengong456@qq.com</p>
+  </Wrapper>
 ));
