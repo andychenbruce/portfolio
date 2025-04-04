954bfd69bbbdfb4982aa60a39b570271e815d489
陈功 <andy@andycbruce.com>
2025-04-03 22:24:40 -0700

    unformat boltzman distribution

--- html_generator/pages/projects/maxwell_boltzman_distribution.tsx
+++ html_generator/pages/projects/maxwell_boltzman_distribution.tsx
@@ -27,33 +27,16 @@ function Boltzman({title}: {title: string}) {
       <h2>Thermodynamics simulation from AP chem</h2>
       <p>
         {" "}
-        When atoms or molecules bounce they are basically exchange velocity
-        equivelent to an elastic collision{" "}
+               When atoms or molecules bounce they are basically exchange velocity
+               equivelent to an elastic collision{" "}
       </p>
       <br />
       <p>
-        I got the equation for 3d elastic collissions of spheres from{" "}
-        <a href="https://www.sjsu.edu/faculty/watkins/collision.htm">here</a>, I
-        don't really understand the proof other than the mass, momentum, and
-        energy conservation equations but I implemented the vector math and it
-        works. There is also a similar equation from this{" "}
-        <a href="https://en.wikipedia.org/wiki/Elastic_collision#Two-dimensional">
-          wikipedia page
-        </a>{" "}
-        on 2d elastic collissions, but because they just use vectors as a whole
-        and not their individual compnents, applying the same equation to 3d
-        also works perfectly, intentional or not. I only use the first one
-        because it's proof is based of thermodynamics and kinetic energy but
-        they are probably the exact same equation just in different form.
+           I got the equation for 3d elastic collissions of spheres from <a href="https://www.sjsu.edu/faculty/watkins/collision.htm">here</a>, I don't really understand the proof other than the mass, momentum, and energy conservation equations but I implemented the vector math and it works. There is also a similar equation from this <a href="https://en.wikipedia.org/wiki/Elastic_collision#Two-dimensional"> wikipedia page</a> on 2d elastic collissions, but because they just use vectors as a whole and not their individual compnents, applying the same equation to 3d also works perfectly, intentional or not. I only use the first one because it's proof is based of thermodynamics and kinetic energy but they are probably the exact same equation just in different form.
       </p>
       <br />
-      <br />
       <p>
-        The{" "}
-        <a href="https://en.wikipedia.org/wiki/Maxwell%E2%80%93Boltzmann_distribution#Distribution_function">
-          equation
-        </a>{" "}
-        for the maxwell boltzman distribution is
+           The <a href="https://en.wikipedia.org/wiki/Maxwell%E2%80%93Boltzmann_distribution#Distribution_function">equation</a> for the maxwell boltzman distribution is
       </p>
       <MakeMathDisplay
         tex={
@@ -61,7 +44,7 @@ function Boltzman({title}: {title: string}) {
         }
       />
       <br />
-      where
+      <p>where</p>
       <br />
       <MakeMath tex={"f(v)"} /> is the probability
       <br />
@@ -74,21 +57,15 @@ function Boltzman({title}: {title: string}) {
       <MakeMath tex={"T"} /> is the temperature
       <br />
       <p>
-        the blue graph isn't actually exactly the correct equation for this
-        system since the units didn't convert nicely when I did it.
+           the blue graph isn't actually exactly the correct equation for this system since the units didn't convert nicely when I did it.
       </p>
       <br />
       <p>
-        the bar graph starts form a velocity of zero and each bucket is 1/700
-        distance units per 1/30th of a second since it does one physics tick per
-        frame which is 1/30th of a second. The units of velocity are quite small
-        since one unit of distance is one square on the cage, or see it as the
-        floor is 4x4 units
+           the bar graph starts form a velocity of zero and each bucket is 1/700 distance units per 1/30th of a second since it does one physics tick per frame which is 1/30th of a second. The units of velocity are quite small since one unit of distance is one square on the cage, or see it as the floor is 4x4 units.
       </p>
       <br />
       <p>
-        basically just shows the maxwell botzman distribution as an emergent
-        propery
+           basically just shows the maxwell botzman distribution as an emergentpropery
       </p>
       <hr className="clearLeft" />
     </Wrapper>
