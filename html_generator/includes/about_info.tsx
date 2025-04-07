0c6ee73506fcff6cc04b68994bb66492e981ee23
陈功 <andy@andycbruce.com>
2025-04-07 16:26:54 -0700

    update about info

--- html_generator/includes/about_info.tsx
+++ html_generator/includes/about_info.tsx
@@ -3,9 +3,22 @@ import React from "react";
 function AboutInfo() {
   return (
     <>
-      { /* <img className="portrait" src="/imgs/Andy-HPA.jpg" /> */ }
       <p>
-        Welcome to my website. I am majoring in computer science at <a href="https://ucsc.edu">UCSC</a> in Santa Cruz, California. This website has many projects that I have worked on in the past few years. My interests include programming, electronics, robotics, artificial intelligence, embedded systems, 3D-graphics, physical simulations, and sustainable agriculture. If you want to hire for machine learning, software engineering, or embedded programming, please look at <a href="/resume.pdf">my resume</a>.
+           Welcome to my website. I am majoring in computer science at <a href="https://ucsc.edu">UCSC</a> in Santa Cruz, California. This website has many projects that I have worked on in the past few years. My interests include:
+      </p>
+      <ul>
+	<li>Software engineering</li>
+	<li>Deductive systems</li>
+	<li>Martin-Löf type theory</li>
+	<li>Homotopy type theory</li>
+	<li>Zermelo–Fraenkel set theory</li>
+	<li>Category theory</li>
+	<li>Statistical mechanics</li>
+	<li>Relativistic quantum mechanics</li>
+	<li>General relativity</li>
+      </ul>
+      <p>
+	   If you want to hire me for machine learning, software engineering, or embedded programming, please look at <a href="/resume.pdf">my resume</a>.
       </p>
     </>
   );
