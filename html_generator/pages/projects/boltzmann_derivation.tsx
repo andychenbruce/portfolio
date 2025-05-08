25ff69b10587302cf44ef52e506c7fd902cd8dd2
陈功 <andy@andycbruce.com>
2025-05-07 22:03:10 -0700

    swap boltzmann definitions

--- html_generator/pages/projects/boltzmann_derivation.tsx
+++ html_generator/pages/projects/boltzmann_derivation.tsx
@@ -6,6 +6,38 @@ import Wrapper from "../../wrapper.js";
 function DeltaGcode({title}: {title: string}) {
   return (
     <Wrapper title={title}>
+      <h2>Derivation by the Limit of the Bath's Heat Capacity In the Canonical Ensemble</h2>
+      <p>In the Canonical ensemble the combined energy of the bath and system should be constant. The combination of the 2 makes a microcanonical ensemble. Let <MakeMath tex={"W_{\\text{total}}(E_{\\text{total}})"} /> be the number of accessable states when the total energy is equal to <MakeMath tex={"E_{\\text{total}}"}/>, and the same with <MakeMath tex={"W_{\\text{bath}}"} /> and <MakeMath tex={"W_{\\text{sys}}"} />. Let the set of microstates of the system can be in <MakeMath tex={"X_{\\text{sys}}"} />, the microstates the bath can be in <MakeMath tex={"X_{\\text{bath}}"} />, and the total microstates <MakeMath tex={"X_{\\text{total}} = X_{\\text{bath}} \\times X_{\\text{sys}}"} />. The total energy should be the sum of the two energies plus a coupling energy.</p>
+      <MakeMathDisplay tex={"E_{\\text{total}} = E_{\\text{bath}} + E_{\\text{sys}} + E_{\\text{coupling}}"} />
+      <p>The total accessable states with an energy should be the total number combined number of states the bath and system can be in that sum to that energy.</p>
+      <MakeMathDisplay tex={"W_{\\text{total}}(E_{\\text{total}}) = \\sum_{r \\in X} W_{\\text{bath}}(E_{\\text{total}} - E_{\\text{sys}}(r))"} />
+      <p>And in the microcanonical ensemble all microstates <MakeMath tex={"(r_\\text{bath}, r_\\text{sys}) \\in X_{\\text{total}}"} /> with the correct have an equal probaility.</p>
+      <MakeMathDisplay tex={"p(r_\\text{bath}, r_\\text{sys}) = \\frac{1}{W_{\\text{total}}(E_{\\text{total}})} \\mathbb{1}\\Big(E_\\text{total} = E_\\text{sys}(r_\\text{sys}) + E_\\text{bath}(r_\\text{bath})\\Big)"} />
+      <p>We can calculate the probability of just the microstate for the system.</p>
+      <MakeMathDisplay tex={"p(r_\\text{sys}) = \\sum_{r_\\text{bath} \\in X_\\text{bath}}p(r_\\text{bath}, r_\\text{sys})"} />
+      <MakeMathDisplay tex={"= \\sum_{r_\\text{bath} \\in X_\\text{bath}} \\frac{1}{W_{\\text{total}}(E_{\\text{total}})} \\mathbb{1}\\Big(E_\\text{total} = E_\\text{sys}(r_\\text{sys}) + E_\\text{bath}(r_\\text{bath})\\Big)"} />
+      <MakeMathDisplay tex={"= \\frac{W_\\text{bath}(E_\\text{total} - E_\\text{sys}(r_\\text{sys}))}{W_{\\text{total}}(E_{\\text{total}})}"} />
+      <p>Take the natural log of both sides</p>
+      <MakeMathDisplay tex={"\\ln(p(r_\\text{sys}))= \\ln(\\frac{W_\\text{bath}(E_\\text{total} - E_\\text{sys}(r_\\text{sys}))}{W_{\\text{total}}(E_{\\text{total}})})"} />
+      <MakeMathDisplay tex={"\\ln(p(r_\\text{sys}))= \\ln(W_\\text{bath}(E_\\text{total} - E_\\text{sys}(r_\\text{sys}))) - \\ln(W_{\\text{total}}(E_{\\text{total}}))"} />
+      <p>Then we expand the logarithm to its Taylor series.</p>
+      <MakeMathDisplay tex={"f(x+c)= f(x) + \\frac{df(x)}{dx}c + \\frac{1}{2}\\frac{d^2f(x)}{dx^2}c^2 + \\frac{1}{6}\\frac{d^3f(x)}{dx^3}c^3 + \\cdots "} />
+      <p>We set <MakeMath tex={"f(x) = \\ln(W_\\text{bath}(x))"} />, <MakeMath tex={"x = E_\\text{total}"} />, and <MakeMath tex={"c = -E_\\text{sys}"} /> we get the following expression.</p>
+      <MakeMathDisplay tex={"\\ln(p(r_\\text{sys})) = \\ln(W_\\text{bath}(E_\\text{total})) - \\frac{d\\ln(W_\\text{bath}(E_\\text{total}))}{dE_\\text{total}}E_\\text{sys}(r_\\text{sys})"} />
+      <MakeMathDisplay tex={"+ \\frac{1}{2}\\frac{d^2\\ln(W_\\text{bath}(E_\\text{total}))}{dE_\\text{total}^2}E_\\text{sys}(r_\\text{sys})^2 - \\frac{1}{6}\\frac{d^3\\ln(W_\\text{bath}(E_\\text{total}))}{dE_\\text{total}^3}E_\\text{sys}(r_\\text{sys})^3 + \\cdots"} />
+      <MakeMathDisplay tex={"\\cdots - \\ln(W_{\\text{total}}(E_{\\text{total}}))"} />
+      <p>In the limit that <MakeMath tex={"E_\\text{total}"} /> approaches <MakeMath tex={"E_\\text{bath}"} /> the bath becomes a microcanonical ensemble and the entropy of the bath <MakeMath tex={"S_\\text{bath} (E_\\text{total}) = S_\\text{bath} (E_\\text{bath}) = k_B\\ln(W_\\text{bath}(E_\\text{bath}))"} /></p>
+      <MakeMathDisplay tex={"= \\ln(W_\\text{bath}(E_\\text{bath})) - \\frac{1}{k_B}\\frac{dS_\\text{bath}(E_\\text{bath})}{dE_\\text{bath}}E_\\text{sys}(r_\\text{sys}) + \\frac{1}{2k_B}\\frac{d^2S_\\text{bath}(E_\\text{bath})}{dE_\\text{bath}^2}E_\\text{sys}(r_\\text{sys})^2"} />
+      <MakeMathDisplay tex={"- \\frac{1}{6k_B}\\frac{d^3S_\\text{bath}(E_\\text{bath})}{dE_\\text{bath}^3}E_\\text{sys}(r_\\text{sys})^3 + \\cdots - \\ln(W_{\\text{total}}(E_{\\text{total}}))"} />
+      <p>And by definition <MakeMath tex={"\\frac{1}{T_\\text{bath}} = \\frac{dS_\\text{bath}(E)}{dE}"} />.</p>
+      <MakeMathDisplay tex={"= \\ln(W_\\text{bath}(E_\\text{bath})) - \\frac{1}{k_B T_\\text{bath}}E_\\text{sys}(r_\\text{sys}) + \\frac{1}{2k_B T_\\text{bath}}\\frac{dT_\\text{bath}}{dE_\\text{bath}^2}E_\\text{sys}(r_\\text{sys})^2"} />
+      <MakeMathDisplay tex={"- \\frac{1}{6k_B T_\\text{bath}}\\frac{d^2T_\\text{bath}}{dE_\\text{bath}^2}E_\\text{sys}(r_\\text{sys})^3 + \\cdots - \\ln(W_{\\text{total}}(E_{\\text{total}}))"} />
+      <p>Then if we assume the heat capacity of the bath is infinite so that <MakeMath tex={"\\frac{dT_\\text{bath}}{dE_\\text{bath}} = 0"} /> we can remove the extra terms.</p>
+      <MakeMathDisplay tex={"\\ln(p(r_\\text{sys})) = - \\frac{1}{k_B T_\\text{bath}}E_\\text{sys}(r_\\text{sys}) + \\ln \\Big(\\frac{W_\\text{bath}(E_\\text{bath})}{W_{\\text{total}}(E_{\\text{total}})} \\Big)"} />
+      <p>Then exponentiate both sides.</p>
+      <MakeMathDisplay tex={"p(r_\\text{sys})= \\frac{e^{-\\frac{E_\\text{sys}(r_\\text{sys})}{k_B T_\\text{bath}}}}{\\Big(\\frac{W_\\text{total}(E_\\text{total})}{W_{\\text{bath}}(E_{\\text{bath}})} \\Big)}"} />
+      <p>And the denominator is just a normalizing factor so the final expression is below.</p>
+      <MakeMathDisplay tex={"p(r_\\text{sys})= \\frac{e^{-\\frac{E_\\text{sys}(r_\\text{sys})}{k_B T_\\text{bath}}}} {\\displaystyle \\sum_{ r\'_\\text{sys} \\in X_\\text{sys}} e^{-\\frac{E_\\text{sys}(r\'_\\text{sys})}{k_B T_\\text{bath}}} }"} />
       <h2>Derivation of the Boltzmann Distribution by Maximizing Entropy</h2>
       <p>Assume there are <MakeMath tex={"N"} /> discrete states the system can be in: <MakeMath tex={"X = \\{ x_1 \\dots x_N \\}"} />. Let <MakeMath tex={"p_i = P(X = x_i)"} /> sampling the state over an indefinite time period, and <MakeMath tex={"E_i"} /> be the energy in state <MakeMath tex={"x_i"} />. The system will have 2 contraints, the probabilities must add to 1, and the average energy, the internal energy, should equal <MakeMath tex={"U"} />.</p>
       <MakeMathDisplay tex={"\\sum_{i=1}^{N} p_i = 1"} />
@@ -80,40 +112,10 @@ function DeltaGcode({title}: {title: string}) {
       <MakeMathDisplay tex={"\\frac{\\partial S_{\\text{max}}}{\\partial U} = \\lambda_2"} />
       <p>So we have this expression which is the thermodynamic temperature.</p>
       <MakeMathDisplay tex={"\\lambda_2  = \\dfrac{\\partial S_{\\text{max}}}{\\partial U} = \\frac{1}{T}"} />
-      <p>I'm not sure if this part is valid because you define <MakeMath tex={"S_\\text{max}"} /> with <MakeMath tex={"\\lambda_2"} /> but then define <MakeMath tex={"\\lambda_2"} /> as the derivative of <MakeMath tex={"S_\\text{max}"} /> which I think creates a circular definition.</p>
+      <p>Because you define <MakeMath tex={"S_\\text{max}"} /> with <MakeMath tex={"\\lambda_2"} /> but then define <MakeMath tex={"\\lambda_2"} /> as the derivative of <MakeMath tex={"S_\\text{max}"} /> which creates a circular definition. For this part to be valid you must already know either the temperature (canonical ensemble) or the average energy.</p>
       <hr/>
       <p>Substituting, the full equation becomes</p>
       <MakeMathDisplay tex={"p_i = \\frac{e^{\\frac{1}{k_B T} E_i}}{ \\sum_{i=1}^{N} e^{\\frac{1}{k_B T} E_i}}"} />
-      <h2>Derivation by the Limit of the Heat Bath's Heat Capacity In the Canonical Ensemble</h2>
-      <p>In the Canonical ensemble the combined energy of the bath and system should be constant. The combination of the 2 makes a microcanonical ensemble. Let <MakeMath tex={"W_{\\text{total}}(E_{\\text{total}})"} /> be the number of accessable states when the total energy is equal to <MakeMath tex={"E_{\\text{total}}"}/>, and the same with <MakeMath tex={"W_{\\text{bath}}"} /> and <MakeMath tex={"W_{\\text{sys}}"} />. Let the set of microstates of the system can be in <MakeMath tex={"X_{\\text{sys}}"} />, the microstates the bath can be in <MakeMath tex={"X_{\\text{bath}}"} />, and the total microstates <MakeMath tex={"X_{\\text{total}} = X_{\\text{bath}} \\times X_{\\text{sys}}"} />. The total energy should be the sum of the two energies.</p>
-      <MakeMathDisplay tex={"E_{\\text{total}} = E_{\\text{bath}} + E_{\\text{sys}}"} />
-      <p>The total accessable states with an energy should be the total number combined number of states the bath and system can be in that sum to that energy.</p>
-      <MakeMathDisplay tex={"W_{\\text{total}}(E_{\\text{total}}) = \\sum_{r \\in X} W_{\\text{bath}}(E_{\\text{total}} - E_{\\text{sys}}(r))"} />
-      <p>And in the microcanonical ensemble all microstates <MakeMath tex={"(r_\\text{bath}, r_\\text{sys}) \\in X_{\\text{total}}"} /> with the correct have an equal probaility.</p>
-      <MakeMathDisplay tex={"p(r_\\text{bath}, r_\\text{sys}) = \\frac{1}{W_{\\text{total}}(E_{\\text{total}})} \\mathbb{1}\\Big(E_\\text{total} = E_\\text{sys}(r_\\text{sys}) + E_\\text{bath}(r_\\text{bath})\\Big)"} />
-      <p>We can calculate the probability of just the microstate for the system.</p>
-      <MakeMathDisplay tex={"p(r_\\text{sys}) = \\sum_{r_\\text{bath} \\in X_\\text{bath}}p(r_\\text{bath}, r_\\text{sys})"} />
-      <MakeMathDisplay tex={"= \\sum_{r_\\text{bath} \\in X_\\text{bath}} \\frac{1}{W_{\\text{total}}(E_{\\text{total}})} \\mathbb{1}\\Big(E_\\text{total} = E_\\text{sys}(r_\\text{sys}) + E_\\text{bath}(r_\\text{bath})\\Big)"} />
-      <MakeMathDisplay tex={"= \\frac{W_\\text{bath}(E_\\text{total} - E_\\text{sys}(r_\\text{sys}))}{W_{\\text{total}}(E_{\\text{total}})}"} />
-      <p>Take the natural log of both sides</p>
-      <MakeMathDisplay tex={"\\ln(p(r_\\text{sys}))= \\ln(\\frac{W_\\text{bath}(E_\\text{total} - E_\\text{sys}(r_\\text{sys}))}{W_{\\text{total}}(E_{\\text{total}})})"} />
-      <MakeMathDisplay tex={"\\ln(p(r_\\text{sys}))= \\ln(W_\\text{bath}(E_\\text{total} - E_\\text{sys}(r_\\text{sys}))) - \\ln(W_{\\text{total}}(E_{\\text{total}}))"} />
-      <p>Then we expand the logarithm to its Taylor series.</p>
-      <MakeMathDisplay tex={"f(x+c)= f(x) + \\frac{df(x)}{dx}c + \\frac{1}{2}\\frac{d^2f(x)}{dx^2}c^2 + \\frac{1}{6}\\frac{d^3f(x)}{dx^3}c^3 + \\cdots "} />
-      <p>We set <MakeMath tex={"f(x) = \\ln(W_\\text{bath}(x))"} />, <MakeMath tex={"x = E_\\text{total}"} />, and <MakeMath tex={"c = -E_\\text{sys}"} /> we get the following expression.</p>
-      <MakeMathDisplay tex={"\\ln(p(r_\\text{sys}))= - \\frac{d\\ln(W_\\text{bath}(E_\\text{total}))}{dE_\\text{total}}E_\\text{sys}(r_\\text{sys}) + \\frac{1}{2}\\frac{d^2\\ln(W_\\text{bath}(E_\\text{total}))}{dE_\\text{total}^2}E_\\text{sys}(r_\\text{sys})^2 + \\frac{1}{6}\\frac{d^3\\ln(W_\\text{bath}(E_\\text{total}))}{dE_\\text{total}^3}E_\\text{sys}(r_\\text{sys})^3 + \\cdots - \\ln(W_{\\text{total}}(E_{\\text{total}}))"} />
-      <p>In the limit that <MakeMath tex={"E_\\text{total}"} /> approaches <MakeMath tex={"E_\\text{bath}"} /> the bath becomes a microcanonical ensemble and the entropy of the bath <MakeMath tex={"S_\\text{bath} (E_\\text{total}) = k_B\\ln(W_\\text{bath}(E_\\text{total}))"} /></p>
-      <MakeMathDisplay tex={"\\ln(p(r_\\text{sys}))= - \\frac{1}{k_B}\\frac{dS_\\text{bath}(E_\\text{total})}{dE_\\text{total}}E_\\text{sys}(r_\\text{sys}) + \\frac{1}{2k_B}\\frac{d^2S_\\text{bath}(E_\\text{total})}{dE_\\text{total}^2}E_\\text{sys}(r_\\text{sys})^2 + \\frac{1}{6k_B}\\frac{d^3S_\\text{bath}(E_\\text{total})}{dE_\\text{total}^3}E_\\text{sys}(r_\\text{sys})^3 + \\cdots - \\ln(W_{\\text{total}}(E_{\\text{total}}))"} />
-      <p>And by definition <MakeMath tex={"\\frac{1}{T_\\text{bath}} = \\frac{dS_\\text{bath}(E)}{dE}"} />.</p>
-      <MakeMathDisplay tex={"\\ln(p(r_\\text{sys}))= - \\frac{1}{k_B T_\\text{bath}}E_\\text{sys}(r_\\text{sys}) + \\frac{1}{2k_B T_\\text{bath}}\\frac{dT_\\text{bath}}{dE_\\text{total}^2}E_\\text{sys}(r_\\text{sys})^2 - \\frac{1}{6k_B T_\\text{bath}}\\frac{d^2T_\\text{bath}}{dE_\\text{total}^2}E_\\text{sys}(r_\\text{sys})^3 + \\cdots - \\ln(W_{\\text{total}}(E_{\\text{total}}))"} />
-      <p>Then if we assume the heat capacity of the bath is infinite so that <MakeMath tex={"\\frac{dT_\\text{bath}}{dE_\\text{total}} = 0"} /> we can remove the extra terms.</p>
-      <MakeMathDisplay tex={"\\ln(p(r_\\text{sys}))= - \\frac{1}{k_B T_\\text{bath}}E_\\text{sys}(r_\\text{sys}) - \\ln(W_{\\text{total}}(E_{\\text{total}}))"} />
-      <p>Then exponentiate both sides.</p>
-      <MakeMathDisplay tex={"p(r_\\text{sys})= \\frac{e^{-\\frac{E_\\text{sys}(r_\\text{sys})}{k_B T_\\text{bath}}}}{W_{\\text{total}}(E_{\\text{total}})}"} />
-      <p>And the denominator is just a normalizing factor.</p>
-      <MakeMathDisplay tex={"\\frac{1}{W_{\\text{total}}(E_{\\text{total}})} = p(r_\\text{bath}\', r_\\text{sys}\')"} />
-      <p>So the final expression is below.</p>
-      <MakeMathDisplay tex={"p(r_\\text{sys})= \\frac{e^{-\\frac{E_\\text{sys}(r_\\text{sys})}{k_B T_\\text{bath}}}} {\\displaystyle \\sum_{ r\'_\\text{sys} \\in X_\\text{sys}} e^{-\\frac{E_\\text{sys}(r\'_\\text{sys})}{k_B T_\\text{bath}}} }"} />
     </Wrapper>
   );
 }
